c4e835608172b98deeb1fac4957aa4fdc2bdd6a7
Luana Salmito <119638705+LuanaSalmito@users.noreply.github.com>
2024-10-27 21:39:06 -0300

    :sparkles: Ajustando rotas, views e config do swagger

---
+++ backend/dama/api/views/ong.py
@@ -0,0 +1,15 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets
+from api.models import ONG
+from rest_framework.permissions import IsAuthenticated
+from api.permissions.todos import TodosPodemCriar, TodosPodemVer
+from api.serializers.ong import ONG
+
+
+class ONGViewSet(viewsets.ModelViewSet):
+    serializer_class = ONG
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete']
+    filter_backends = [DjangoFilterBackend]
+    permission_classes = [IsAuthenticated & (TodosPodemCriar | TodosPodemVer)]
+    queryset = ONG.objects.all()
+
---
+++ backend/dama/api/views/usuario.py
@@ -0,0 +1,24 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets
+from api.models import Usuario
+from rest_framework.permissions import IsAuthenticated
+from api.permissions.todos import TodosPodemCriar
+from api.serializers.usuario import UsuarioSerializer
+from rest_framework.authentication import TokenAuthentication
+
+
+
+class UsuarioViewSet(viewsets.ModelViewSet):
+    serializer_class = UsuarioSerializer
+    http_method_names = ['get', 'post']
+    filter_backends = [DjangoFilterBackend]
+    permission_classes = [IsAuthenticated | TodosPodemCriar]
+    authentication_classes = [TokenAuthentication] 
+    queryset = Usuario.objects.all()
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(usuario=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(usuario=self.request.user)
